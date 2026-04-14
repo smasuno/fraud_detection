91a28ad40d100b9eb82e293d92c29cb5c66981df
Shintaro Masuno <shintaromasuno@gmail.com>
2026-04-14 09:40:12 -0700

    made changes to notebook

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import precision_recall_curve, PrecisionRecallDisplay, ConfusionMatrixDisplay, confusion_matrix
 import numpy as np
 import shap
+import seaborn as sns
 
 def eda_plot1(df) -> plt.figure:
     eda_df = df.groupby(['Time', 'Class']).apply(lambda x: x.shape[1]).reset_index()
@@ -23,6 +24,17 @@ def eda_plot1(df) -> plt.figure:
 
     return fig
 
+def score_distribution_plot(y_pred, y_valid) -> plt.figure:
+    plot_df = pd.DataFrame({
+        'score': y_pred,
+        'class': y_valid
+    })
+    plot_df['label'] = np.where(plot_df['class']==1, 'Fraud', 'Legitimate')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.histplot(data=plot_df, x='score', hue='label', stat='probability', common_norm=False, bins=50, alpha=0.5, ax=ax)
+    ax.set_xlabel("Predicted Score")
+    ax.set_title("Score Distribution by Class (Validation Set)")
+    return None
 
 def beeswarm_plot(X_train, model) -> plt.figure:
     #fig, ax = plt.subplots(figsize=(12, 8))
